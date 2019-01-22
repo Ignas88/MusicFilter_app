fb3597985e75a729f6a7b3eeb9c08352cd98cb78
Ignas <zigizmas@gmail.com>
2019-01-22 14:00:40 +0200

    styling finished

---
+++ src/components/sidebar/Form.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+class Form extends Component {
+  state = {
+    checkedA: true,
+    checkedB: true,
+    checkedF: true,
+    checkedG: true,
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.checked });
+  };
+
+  render() {
+
+    return (
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.checkedB}
+              onChange={this.handleChange('checkedB')}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Primary"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.checkedB}
+              onChange={this.handleChange('checkedB')}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Primary"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.checkedB}
+              onChange={this.handleChange('checkedB')}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Primary"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={this.state.checkedB}
+              onChange={this.handleChange('checkedB')}
+              value="checkedB"
+              color="primary"
+            />
+          }
+          label="Primary"
+        />
+      </FormGroup>
+    );
+  }
+}
+
+export default Form;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import Form from './Form';
 
 const drawerWidth = 300;
 
 const styles = theme => ({
   root: {
     display: 'flex',
+  },
+  paper: {
+    margin: 20,
+    padding: '20px 25px 13px 25px'
   },
   drawer: {
     width: drawerWidth,
@@ -16,7 +24,13 @@ const styles = theme => ({
   drawerPaper: {
     width: drawerWidth,
   },
-  toolbar: theme.mixins.toolbar
+  toolbar: theme.mixins.toolbar,
+  sideHeader: {
+    height: 65,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 class Sidebar extends Component {
@@ -34,6 +48,18 @@ class Sidebar extends Component {
           anchor="left"
         >
           <div className={classes.toolbar}>
+            <div className={classes.sideHeader}>
+              <Typography variant="h4" color="textSecondary">Filter</Typography>
+            </div>
+            <Divider />
+            <Paper className={classes.paper} elevation={1}>
+              <Typography variant="subtitle2">by Price:</Typography>
+              <Form />
+            </Paper>
+            <Paper className={classes.paper} elevation={1}>
+              <Typography variant="subtitle2">by Year:</Typography>
+              <Form />
+            </Paper>
           </div>
         </Drawer>
       </div>
